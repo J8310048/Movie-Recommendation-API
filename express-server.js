3ff024d73d17a15947be39fac925286ecd3922f6
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2024-06-16 15:47:46 -0400

    finished the functionality, now I'm working on the styling

--- express-server.js
+++ express-server.js
@@ -5,11 +5,13 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
-app.use(cors()); //this is the middleware/middleman
+app.use(cors());
 
-app.get("/", (req, res) => {
-  const url =
-    "https://api.themoviedb.org/3/search/movie?query=Batman&include_adult=false&language=en-US&page=1";
+app.get("/search", (req, res) => {
+  const query = req.query.query;
+  const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+    query
+  )}&include_adult=false&language=en-US&page=1`;
   const options = {
     method: "GET",
     headers: {
@@ -22,7 +24,23 @@ app.get("/", (req, res) => {
     .then((resp) => resp.json())
     .then((json) => res.send(json))
     .catch((err) => console.error("error:" + err));
-  //   res.send({ id: "Joshua" });
+});
+
+app.get("/similar", (req, res) => {
+  const movieId = req.query.movie_id;
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.TMDB_API_TOKEN,
+    },
+  };
+
+  fetch(url, options)
+    .then((resp) => resp.json())
+    .then((json) => res.send(json))
+    .catch((err) => console.error("error:" + err));
 });
 
 app.listen(port, () => {
